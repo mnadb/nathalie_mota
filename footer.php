2ac5894c77d1b3b39feae314af4690c228d9139e
Marie <mnad.contact@gmail.com>
2026-06-01 16:11:19 +0200

    Mise en place de la page de Politique de confidentialité

--- footer.php
+++ footer.php
@@ -1,17 +1,32 @@
- <footer class="menu_footer">
-         <?php
-        wp_nav_menu(array(
-            'theme_location' => 'footer',
-            'container'      => false,
-            'menu_class'     => 'menu',
-        ));
-    ?>
+
+<!--Récupération de l'URL de la politique de confidentialité -->
+<?php
+$privacy_url = get_privacy_policy_url();
+?>
+
+<footer class="menu_footer">
+    <ul class="menu">
+        <li>
+            <a href="<?php echo esc_url(home_url('/mentions-legales/')); ?>">Mentions légales</a>
+        </li>
+<!-- vérification l'existance du lien -->
+        <?php if ($privacy_url) : ?>
+            <li>
+                <a href="<?php echo esc_url($privacy_url); ?>">Vie privée</a>
+            </li>
+        <?php endif; ?>
+
+        <li>
+            <span>Tous droits réservés</span>
+        </li>
+    </ul>
+   
+
+</footer>
+
 <?php get_template_part('parts/contact-popup'); ?>
 
 <?php wp_footer(); ?>
-</footer>
-<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
 </body>
 </html>
--- page.php
+++ page.php
@@ -16,6 +16,13 @@
     <?php if ( have_posts() ) :
 	 while ( have_posts() ) : 
 	 	the_post(); ?>
+        <section class="privacy_policy">
+         <h1><?php the_title(); ?></h1>
+
+        <div class="page-content">
+            <?php the_content(); ?>
+        </div>
+     </section>
     <?php endwhile; endif; ?>
 	
 </main>
